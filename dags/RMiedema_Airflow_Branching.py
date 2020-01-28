dbe0cd63ed6fbe613a94b76e43aa54eb5267086f
rmiedema <57907099+rmiedema@users.noreply.github.com>
2020-01-28 15:25:24 +0100

    Add files via upload

---
+++ dags/RMiedema_Airflow_Branching.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+import airflow
+from datetime import datetime
+from datetime import timedelta
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python_operator import BranchPythonOperator
+
+
+# In[19]:
+
+
+def _get_weekday(execution_date, **context):
+    return execution_date.strfstime("%a")
+
+
+# In[20]:
+
+
+args = {
+    'owner': 'R_Miedema',
+    'depends_on_past': False,
+    'start_date': datetime(2020, 1, 1),
+    'email': ['rmiedema@bol.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+}
+
+
+# In[21]:
+
+
+dag = DAG(
+    dag_id='miedema_exercise_V',
+    default_args=args,
+    schedule_interval= None,
+    dagrun_timeout=timedelta(minutes=5),
+)
+
+
+# In[28]:
+
+
+task_1 = DummyOperator(
+    task_id='run_this_first',
+    dag = dag,
+)
+
+task_2 = DummyOperator(
+    task_id='run_this_second',
+    dag = dag,
+)
+
+branching = BranchPythonOperator(
+        task_id="branching", 
+        python_callable=_get_weekday, 
+        provide_context=True,
+        dag=dag,
+)
+
+join = DummyOperator(
+    task_id="final_task",
+    trigger_rule = "none_failed",
+    dag=dag,
+)
+
+task_1 >> task_2 >> branching >> join
+
+
+# In[ ]:
+
+
+
+
