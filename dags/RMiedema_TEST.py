7acef3f3dcd87dbdb6b230d16497818afa2456fe
rmiedema <57907099+rmiedema@users.noreply.github.com>
2020-01-28 13:11:51 +0100

    Update RMiedema_TEST.py

--- dags/RMiedema_TEST.py
+++ dags/RMiedema_TEST.py
@@ -1,44 +1,28 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[12]:
-
-
 import airflow
 from datetime import timedelta
 from airflow.models import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
 
-
-# In[13]:
-
-
 args = {
     'owner': 'R_Miedema',
     'depends_on_past': False,
-    'start_date': airflow.utils.dates.days_ago(2),
+    'start_date': datetime(2020, 1, 1),
     'email': ['rmiedema@bol.com'],
-    'email_on_success' : True,
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
 }
 
-
-# In[18]:
-
-
 dag = DAG(
     dag_id='miedema_test',
     default_args=args,
-    schedule_interval= None,
+    schedule_interval= '@daily',
     dagrun_timeout=timedelta(minutes=60),
 )
-
-
-# In[21]:
-
 
 t1 = DummyOperator(
     task_id='task1',
@@ -67,8 +51,3 @@ t5 = DummyOperator(
 
 t1 >> t2 >> t3 >> t5
 t2 >> t4 >> t5
-
-
-
-
-
